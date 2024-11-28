ed1a3eff8d94bc0b2e98a625d616524e3c7053e3
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-28 23:53:10 +0700

    styling home activity

--- app/src/main/java/com/c242ps188/mentally_app/ui/view/home/HomeActivity.kt
+++ app/src/main/java/com/c242ps188/mentally_app/ui/view/home/HomeActivity.kt
@@ -14,7 +14,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(0, systemBars.top, 0, 0)
             insets
         }
     }
