0ad5c630a1ac72b6cc89eb0251acb5f9d749e63b
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-29 21:35:46 +0700

    create custom button

--- app/src/main/java/com/c242ps188/mentally_app/ui/view/home/HomeActivity.kt
+++ app/src/main/java/com/c242ps188/mentally_app/ui/view/home/HomeActivity.kt
@@ -14,7 +14,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(0, systemBars.top, 0, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
     }
