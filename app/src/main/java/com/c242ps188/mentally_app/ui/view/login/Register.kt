affc75dfd575c0cb736da67bc6d18a03835f91f2
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-30 08:31:31 +0700

    refactor login

--- app/src/main/java/com/c242ps188/mentally_app/ui/view/login/Register.kt
+++ app/src/main/java/com/c242ps188/mentally_app/ui/view/login/Register.kt
@@ -1,16 +1,17 @@
-package com.C242PS188.mentally_app
+package com.c242ps188.mentally_app.ui.view.login
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.C242PS188.mentally_app.R
 
-class Login : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
