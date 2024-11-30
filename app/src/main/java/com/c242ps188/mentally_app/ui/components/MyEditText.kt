117d938a6e14d87dc5bc31673c70d77f00b9cfab
FuadHmdn <fuadhamidan.drive@gmail.com>
2024-11-30 09:14:46 +0700

    refactor

--- app/src/main/java/com/c242ps188/mentally_app/ui/components/MyEditText.kt
+++ app/src/main/java/com/c242ps188/mentally_app/ui/components/MyEditText.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 import com.C242PS188.mentally_app.R
 
-class MyButton: AppCompatEditText {
+class MyEditText: AppCompatEditText {
 
     constructor(context: Context): super(context)
     constructor(context: Context, attrs: AttributeSet): super(context, attrs)
@@ -23,19 +23,5 @@ class MyButton: AppCompatEditText {
             error = null
         }
     }
-
-//    <com.c242ps188.mentally_app.ui.components.MyButton
-//    android:layout_width="match_parent"
-//    android:layout_marginStart="16dp"
-//    android:layout_marginEnd="16dp"
-//    android:layout_height="60dp"
-//    android:background="@drawable/bg_rounded_edit_text"
-//    app:layout_constraintStart_toStartOf="parent"
-//    android:hint="@string/enter_your_email"
-//    android:paddingStart="24dp"
-//    android:inputType="textPassword"
-//    app:layout_constraintEnd_toEndOf="parent"
-//    app:layout_constraintTop_toBottomOf="@id/rv_article"
-//    tools:ignore="RtlSymmetry" />
 
 }
