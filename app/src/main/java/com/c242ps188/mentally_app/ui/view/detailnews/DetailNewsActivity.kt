f4863753a0e53e8bf72e9d74b590aeed5ec57a9f
LuthfiAditya01 <luthfi2004aditya@gmail.com>
2024-12-11 02:27:44 +0700

    Adding News Detail Layout and Binding

--- app/src/main/java/com/c242ps188/mentally_app/ui/view/detailnews/DetailNewsActivity.kt
+++ app/src/main/java/com/c242ps188/mentally_app/ui/view/detailnews/DetailNewsActivity.kt
@@ -1,13 +1,17 @@
 package com.c242ps188.mentally_app.ui.view.detailnews
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.C242PS188.mentally_app.R
+import com.C242PS188.mentally_app.databinding.ActivityDetailNewsBinding
 
 class DetailNewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailNewsBinding
+
 
 
 
@@ -15,7 +19,12 @@ class DetailNewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_detail_news)
+        binding = ActivityDetailNewsBinding.inflate(layoutInflater)
+
+        binding.tvNewsHeader.text = "Do you or Do you not feel Bonita?"
+        binding.tvDateUpdated.text = "Updated on 1st December 2024"
+        binding.NewsExample.text = "Great, you look Bonita, Gracias cariña"
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
